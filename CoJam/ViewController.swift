0b1d69291e09e06b130bd7a5030885748ec94f00
Ahmed Ibrahim <ahmed@audesis.com>
2017-05-26 15:18:00 -0700

    Initial copy and paste try from old code.

--- CoJam/ViewController.swift
+++ CoJam/ViewController.swift
@@ -6,20 +6,70 @@
 //  Copyright © 2017 Audesis. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var gainValue: UILabel!
+    @IBOutlet weak var upBtn: UIButton!
+    @IBOutlet weak var downBtn: UIButton!
+    @IBOutlet weak var awarenessBtn: UIButton!
+    
+    var onAwareness = false
+    var gain = 5
+    var audioProcessor : AudioProcessor? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.gainValue.text = String(gain)
+        audioProcessor = AudioProcessor()
+        UIApplication.shared.isIdleTimerDisabled = true
     }
-
+    
+    @IBAction func upGain(_ sender: UIButton) {
+        gain += 1
+        audioProcessor?.gain = Float(gain)
+        self.gainValue.text = String(gain)
+        
+    }
+    @IBAction func downGain(_ sender: UIButton) {
+        gain -= 1
+        audioProcessor?.gain = Float(gain)
+        self.gainValue.text = String(gain)
+    }
+    
+    @IBAction func awarenessAction(_ sender: UIButton) {
+        //code to enable touch of button "touch"
+        if onAwareness {
+            print("** onAwareness off **")
+            onAwareness = false;
+            self.awarenessBtn.setTitleColor(UIColor.blue, for: UIControlState.normal)
+            //audioProcessor!.stop()
+            deactiveAwareness()
+        }else{
+            onAwareness = true;
+            print("** onAwareness on **")
+            self.awarenessBtn.setTitleColor(UIColor.red, for: UIControlState.normal)
+            activeAwareness()
+            
+        }
+    }
+    
+    func activeAwareness() {
+        audioProcessor!.start()
+    }
+    
+    
+    func deactiveAwareness(){
+        audioProcessor!.stop()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
